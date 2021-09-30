3ba806b89e03f1a5e0e643c6bfe3915c4911d8ce
Yegor Zhumikov <yegor@yogurtthehor.se>
2021-09-30 13:06:22 +0300

    feat: remove full text from short page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import './App.css';
 import {Layout} from "./Shared/Layout";
 import {MainPage} from "./Pages/Main";
 import {ShortPage} from "./Pages/Short";
+import { Switch } from 'react-router-dom';
 
 function App() {
     return <Layout>
-        <Route exact path='/' component={MainPage}/>
-        <Route exact path='/short' component={ShortPage}/>
-        <Route path="*" component={MainPage} />
+        <Switch>
+            <Route exact path='/' component={MainPage}/>
+            <Route exact path='/short' component={ShortPage}/>
+            <Route path="*" component={MainPage}/>
+        </Switch>
     </Layout>;
 }
 
