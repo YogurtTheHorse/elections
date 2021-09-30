5f25c1ce3d0a9e3a3c1985afc2a877cee3007441
Yegor Zhumikov <yegor@yogurtthehor.se>
2021-09-30 19:32:52 +0300

    fix: typo

--- src/Pages/Placeholder.tsx
+++ src/Pages/Placeholder.tsx
@@ -3,7 +3,7 @@ import {Container, Row} from "react-bootstrap";
 export const Placeholder = () => {
     return <Container className="page-content">
         <Row className="hello">
-            На данный момент, в регламенте есть намек на то, что анитацию нельзя вести до появления
+            На данный момент, в регламенте есть намек на то, что агитацию нельзя вести до появления
             списка кандидтов, поэтому сайт находится в заморозке. Ждите следующей недели.
         </Row>
     </Container>;
