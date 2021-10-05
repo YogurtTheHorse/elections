866d91f0a92b8dd8f9fae8b2318deb0e18d8dfe0
Yegor Zhumikov <yegor@yogurtthehor.se>
2021-10-05 17:11:53 +0300

    Launch

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ import {Placeholder} from "./Pages/Placeholder";
 function App() {
     return <Layout>
         <Switch>
-            {/*<Route exact path='/' component={MainPage}/>*/}
-            {/*<Route exact path='/short' component={ShortPage}/>*/}
-            <Route path="*" component={Placeholder}/>
+            <Route exact path='/' component={MainPage}/>
+            <Route exact path='/short' component={ShortPage}/>
+            <Route path="*" component={MainPage}/>
         </Switch>
     </Layout>;
 }
